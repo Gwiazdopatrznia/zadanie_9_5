1727949a05bdda6f0472337e4b18df481a14e5c2
Anna Gajewska <gajewska.anna@o2.pl>
2018-03-14 22:52:38 +0100

    Add scripts

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var withButtonItemsClass = document.getElementsByClassName('button');
+
+for ( var b = 0 ; b < withButtonItemsClass.length ; b++ ) {
+	
+	console.log(withButtonItemsClass[b].innerText);
+} 
